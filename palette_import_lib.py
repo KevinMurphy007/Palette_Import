68b58b9191d26f690804df7f287cc52d12b0d11a
K Murphy <kemurphy@canterburyschool.org>
2018-12-05 10:52:23 -0500

    working and finished pallette dictionary

--- palette_import_lib.py
+++ palette_import_lib.py
@@ -2,10 +2,20 @@
 functions to read the a CSV file
 and put it into a dictionary
 """
-
-
+import csv
+mydict = dict()
 def read_palette_from_file(palette_filename):
     """
-    read the palette from the csv file and put into a dictionary
+    read the palette from the csv file.
+    Each row is a key and an RGB triple.  Make that RGB triple
+    into a TUPLE and then put into a dictionary with the key.
     """
-    pass
+
+    with open(palette_filename) as csvfile:
+        read= csv.reader(csvfile)
+        for row in read:
+            key = row[0]
+            key = int(key)
+            value = [row[1], row[2], row[3]]
+            mydict[key]= value
+    return mydict
